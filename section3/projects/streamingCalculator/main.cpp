d433abeb127ba7c5945ae4796ef8a1a86259cf66
grant <gthiele2028@berkeley.edu>
2026-01-20 14:13:29 -0800

    first project done

---
+++ section3/projects/streamingCalculator/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    //Take inputs first
+
+    char plan;
+    int devices;
+
+    cout << "Streaming Service Calculator" << endl;
+
+    bool loopChecker = true;
+
+    while (loopChecker) {
+        
+        cout << "Which plan do you use? A, B, or C?" << endl;
+        cin >> plan;
+
+        switch (plan) {
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'a':
+            case 'b':
+            case 'c':
+                cout << "Got it" << endl;
+                loopChecker = false;
+                break;
+            default:
+                cout << "Invalid Plan.  Try Again.";
+                break;
+        }
+    }
+
+    cout << "How many devices do you use at once?" << endl;
+    cin >> devices;
+
+    //Output here
+    int price = 0;
+    int devicesAllowed;
+    int extraPrice;
+
+    switch (plan) {
+            case 'A':
+            case 'a':
+                price += 9;
+                devicesAllowed = 1;
+                extraPrice = 6;
+                break;
+            case 'B':
+            case 'b':
+                price += 14;
+                devicesAllowed = 3;
+                extraPrice = 4;
+                break;
+            case 'C':
+            case 'c':
+                price += 20;
+                devicesAllowed = 5;
+                extraPrice = 2;
+                break;
+            default:
+                cout << "This shouldn't be possible" << endl;
+                break;
+    }
+
+    if (devices > devicesAllowed) {
+        price += extraPrice * (devices - devicesAllowed);
+    }
+
+    cout << "Your total price will be: $" << price << endl;
+
+    return 0;
+}
