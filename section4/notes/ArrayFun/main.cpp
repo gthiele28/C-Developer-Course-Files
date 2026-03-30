57e26144343c020b8015b3b55046fbc7222534bc
grant <gthiele2028@berkeley.edu>
2026-03-30 14:05:52 -0700

    second folder starting

--- section4/notes/ArrayFun/main.cpp
+++ section4/notes/ArrayFun/main.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main() {
 
+    const int ARRAY_SIZE = 5;
+    int myArray[ARRAY_SIZE];
+
+    myArray[0] = 15;
+    myArray[1] = 20;
+    myArray[2] = 22;
+    myArray[3] = 13;
+    myArray[4] = 6;
+
+    //When passing limits of array, garbage data returned but no error given (like C)
+    //"Undefined Behavior"
+    //Can also cause code to crash
+    
+    for (int i = 0; i <= ARRAY_SIZE; i++) {
+        cout << myArray[i] << endl;
+    }
+
     return 0;
 }
