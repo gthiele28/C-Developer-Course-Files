f03937ef6fcc05a9fffe9a03f662a57233502f10
grant <gthiele2028@berkeley.edu>
2026-05-25 18:39:50 -0700

    section 7 part 5

---
+++ section7/notes/CustomExceptions/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "AngryCatException.h"
+using namespace std;
+
+void feedKitty(int numTreats);
+
+int main () {
+    int numTreats = 0;
+    cout << "How many treats do you want to feed the kitty?" << endl;
+    cin >> numTreats;
+
+    try {
+        feedKitty(numTreats);
+    } catch (const AngryCatException& err) {
+        cout << err.what() << endl;
+    }
+}
+
+void feedKitty(int numTreats) {
+    if (numTreats < 3) {
+        throw AngryCatException();
+    } else if (numTreats < 6) {
+        throw AngryCatException("Kitty is disappointed");
+    }
+
+    cout << "Kitty is happy with " << numTreats << " treats." << endl;
+}
