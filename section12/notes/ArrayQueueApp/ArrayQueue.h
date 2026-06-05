05fe45ca2a0e0d2eaa8131851ac3afe0e7795507
grant <gthiele2028@berkeley.edu>
2026-06-04 18:11:43 -0700

    section 12 part 6

---
+++ section12/notes/ArrayQueueApp/ArrayQueue.h
@@ -0,0 +1,72 @@
+#ifndef ARRAY_QUEUE_H
+#define ARRAY_QUEUE_H
+
+#include <iostream>
+#include "Queue.h"
+using namespace std;
+
+class ArrayQueue : public Queue {
+    public:
+        ArrayQueue(int s = 16) : MAX_SIZE(s) {
+            front = 0;
+            back = 0;
+            numElements = 0;
+            mArray = new int[MAX_SIZE];
+        } //end ctor
+
+        virtual ~ArrayQueue() {
+            delete[] mArray;
+        } //end dtor
+
+        void enqueue(int newEntry) override {
+            if (numElements < MAX_SIZE - 1) {
+                mArray[back] = newEntry;
+                back = (back + 1) % MAX_SIZE;
+                numElements++;
+            } else {
+                cout << "Queue is full" << endl;
+                return;
+            }
+        } //end enqueue
+
+        int dequeue() override {
+            if (!isEmpty()) {
+                int data = mArray[front];
+                front = (front + 1) % MAX_SIZE;
+                numElements--;
+                return data;
+            } else {
+                cout << "Queue is empty" << endl;
+                return 0;
+            }
+        } //end dequeue
+
+        int peekFront() const override {
+            if (!isEmpty()) {
+                return mArray[front];
+            } else {
+                cout << "Queue is empty" << endl;
+                return 0;
+            }
+        }
+
+        bool isEmpty() const override {
+            return numElements == 0;
+        } //end isEmpty
+
+        void makeEmpty() override {
+            front = 0;
+            back = 0;
+            numElements = 0;
+        } //end makeEmpty
+
+
+    private:
+        int *mArray;
+        const int MAX_SIZE;
+        int front;
+        int back;
+        int numElements;
+};
+
+#endif
